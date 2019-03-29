dd3e207eceb51d73ec63a611cd7185675c209d42
aliyev12 <aaa719@nyu.edu>
2019-03-29 11:44:16 -0400

    got rid of DOMContentLoaded listener

--- main.js
+++ main.js
@@ -1,35 +1,33 @@
-// Get modal element
-document.addEventListener ('DOMContentLoaded', function () {
-  (function init () {
-    const modal = document.getElementById ('simpleModal');
-    // Get open modal button
-    const modalBtn = document.getElementById ('modalBtn');
-    // Get close button
-    const closeBtn = document.querySelector ('.closeBtn');
+(function init () {
+  // Get modal element
+  const modal = document.getElementById ('simpleModal');
+  // Get open modal button
+  const modalBtn = document.getElementById ('modalBtn');
+  // Get close button
+  const closeBtn = document.querySelector ('.closeBtn');
 
-    // Listen for open click
-    modalBtn.addEventListener ('click', openModal);
+  // Listen for open click
+  modalBtn.addEventListener ('click', openModal);
 
-    // Listen for close click
-    closeBtn.addEventListener ('click', closeModal);
-    // Outside click
-    window.addEventListener('click', clickOutside);
+  // Listen for close click
+  closeBtn.addEventListener ('click', closeModal);
+  // Outside click
+  window.addEventListener ('click', clickOutside);
 
-    // Function to open modal
-    function openModal () {
-      modal.style.display = 'block';
-    }
+  // Function to open modal
+  function openModal () {
+    modal.style.display = 'block';
+  }
 
-    // Function to close modal
-    function closeModal () {
+  // Function to close modal
+  function closeModal () {
+    modal.style.display = 'none';
+  }
+
+  // Function to close modal if outside click
+  function clickOutside (e) {
+    if (e.target === modal) {
       modal.style.display = 'none';
     }
-
-    // Function to close modal if outside click
-    function clickOutside(e) {
-        if (e.target === modal) {
-            modal.style.display = 'none';
-        }
-    }
-  }) ();
-});
+  }
+}) ();
